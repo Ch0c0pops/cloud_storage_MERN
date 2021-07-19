90b73baed41a21c892fee856eec78ecac1e98b16
Ch0c0pops <ch0c0lateroollzz@yandex.ru>
2021-07-19 18:20:53 +0300

    fileList and few others components added, server side files are shown on front end side

--- server/models/File.js
+++ server/models/File.js
@@ -6,6 +6,7 @@ const File = new mongoose.Schema({
     accessLink: {type: String},
    size:{type: Number, default: 0}, // размер в байтах
     path:{type: String, default: ''},
+    date:{type: Date, default: Date.now()},
     user: {type: mongoose.ObjectId, ref: 'User'},// ссылается на пользователя, чей файл
     parent: {type: mongoose.ObjectId, ref: 'File'}, //ссылается на внешнюю папку файла
    child:[{type: mongoose.ObjectId, ref:'File'}] //на все внутренние файлы в папке
